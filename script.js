4b1fdc8953dcff0b8d14d50e788ab25feccf709a
Paula Goyanes <goyanespaula@gmail.com>
2018-02-04 22:52:28 -0800

    added guide dots (do not link)

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 var imgList = [
   {
     src: "https://i.cbc.ca/1.4473485.1515096470!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/637878118.jpg", 
-    name: "Habana, Cuba"}, 
+    name: "Habana, Cuba",
+    page: 1
+  }, 
   {
     src: "https://local-store-res.cloudinary.com/image/upload/w_640,h_480,c_fill,q_80,f_auto/Miami_FL-00006_a8r7h3",
-    name: "Miami, FL, USA"
+    name: "Miami, FL, USA",
+    page: 2
   },  
   {
     src: "https://activerain-store.s3.amazonaws.com/image_store/uploads/6/9/7/4/8/ar129618772984796.JPG",
-    name: "Norwalk, CT, USA"
+    name: "Norwalk, CT, USA",
+    page: 3
   },  
   {
     src: "http://www.optimaproperties.com/wp-content/uploads/2015/01/lake-worth.jpg", 
-    name: "Lake Worth, FL, USA"
+    name: "Lake Worth, FL, USA",
+    page: 4
   }, 
   {
     src: "https://ssl.c.photoshelter.com/img-get2/I0000FrHMVVg7n64/fit=1000x750/Jamal-area-Guantanamo-Cuba-004.jpg", 
-    name: "Guantanamo, Cuba"
+    name: "Guantanamo, Cuba",
+    page: 5
   },
   {
     src: "http://www.visitflorida.com/content/dam/visitflorida/en-us/images/cities/tallahassee/tallahassee%204.jpg.1280.500.rendition", 
-    name: "Tallahassee, FL, USA"
+    name: "Tallahassee, FL, USA",
+    page: 6
   }, 
   {
     src: "http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1446655168/chicago-header-dg1115.jpg?itok=MqZFOaTi", 
-    name: "Chicago, IL, USA"
+    name: "Chicago, IL, USA",
+    page: 7
   }, 
   {
     src: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/06/28/103750292-GettyImages-185111233.jpg?v=1491837696", 
-    name: "San Francisco, CA, USA"
+    name: "San Francisco, CA, USA",
+    page: 8
   }
-]
+];
+
+function createGuides(imgList, $guideWrapper) {
+  var $guide = $("<div>").html('<i class="fas fa-circle"></i>');
+  for (var i = 0; i < imgList.length; i++) {
+    $guide.attr("class", "guide page-" + (i + 1));
+    $guideWrapper.append($guide.clone());
+  }
+}
 
 function renderImages(imgList, $images, $captions) {
+  var pageNum;
+  var $guides = $(".guide");
   for (var i = 0; i < $images.length; i++) {
     $($images[i]).attr({
       src: imgList[i].src,
@@ -40,6 +59,10 @@ function renderImages(imgList, $images, $captions) {
     });
     $($captions[i]).text(imgList[i].name);
   }
+  pageNum = imgList[0].page;
+  $guides.removeClass("active");
+  $activeGuide = $(".guide.page-" + pageNum);
+  $activeGuide.addClass("active");
 };
 
 function scrollRight(imgList, $images, $captions) {
@@ -55,9 +78,11 @@ function scrollLeft(imgList, $images, $captions) {
 $(document).ready(function() {
   var $images = $(".img-wrapper>img");
   var $captions = $(".img-wrapper>figcaption");
-  var $leftButton = $("#left-button");
-  var $rightButton = $("#right-button");
+  var $leftButton = $(".left-button");
+  var $rightButton = $(".right-button");
+  var $guideWrapper = $(".guide-wrapper");
 
+  createGuides(imgList, $guideWrapper);
   renderImages(imgList, $images, $captions);
 
   $rightButton.on("click", function() {
